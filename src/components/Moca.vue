544231da95f44665c057d2f1e0c6569eb6776886
avirads <avirads@gmail.com>
2023-09-05 16:16:57 +0530

    added slots

--- src/components/Moca.vue
+++ src/components/Moca.vue
@@ -4,18 +4,21 @@ import { useSeek } from '@tresjs/core'
 import { useGLTF } from '@tresjs/cientos'
 import { ShaderMaterial } from 'three'
 
-const { nodes } = await useGLTF('8-54-Stagger-moca_rooms_xyz.glb', { draco: true })
+const { nodes } = await useGLTF('rooms_8-54-Stagger-moca_rooms_xyz.glb', { draco: true })
 
-
-const model = nodes.mm_rooms_8slots_modelo
+console.log(nodes)
+const model = nodes.Empty
 
 const { seekByName } = useSeek()
-const outside = seekByName(model,'mm_rooms_8slots_modelo_primitive174')
+
+/*
+const outside = seekByName(model,'Slot_001')
 console.log(outside)
 outside.material = new ShaderMaterial({
       vertexShader: ``,
       fragmentShader:``
 })
+*/
 </script>
 
 <template>
